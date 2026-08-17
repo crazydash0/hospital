28f62ba08ac02c854ee5253db48b2b5d8749cd45
crazydash0 <76502255+crazydash0@users.noreply.github.com>
2026-08-17 16:07:26 +0300

    feat: register clinic owner dashboard

--- backend/src/dashboard/dashboard.module.ts
+++ backend/src/dashboard/dashboard.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DashboardController } from './dashboard.controller';
 import { DashboardService } from './dashboard.service';
+import { ClinicOwnerController } from './clinic-owner.controller';
+import { ClinicOwnerService } from './clinic-owner.service';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [DashboardController],
-  providers: [DashboardService],
+  controllers: [DashboardController, ClinicOwnerController],
+  providers: [DashboardService, ClinicOwnerService],
 })
 export class DashboardModule {}
